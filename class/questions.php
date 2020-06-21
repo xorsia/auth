9665254e051c34a4def15b719d14007752734663
Xorsia <thismailforhacker@gmail.com>
2020-06-21 17:16:52 +0300

    реализован вывод данных - костыль

--- class/questions.php
+++ class/questions.php
@@ -5,15 +5,34 @@ class Questions extends Db{
     public $data = [];
 
     public function GetQuestions(){
+//        $sql = "SELECT * FROM questions LEFT JOIN answers ON questions.id = answers.q_id";
+        $sql = "SELECT * FROM questions";
 
-        $sql = "SELECT * FROM  answers INNER JOIN questions ON answers.q_id = questions.id";
         $result = mysqli_query(Db::connect(), $sql) or die('mysql error');
         $data = mysqli_fetch_all($result);
-        
 
-        $this->data = $data;
-        echo "<pre>";
-        print_r($this->data);
-        exit();
+        foreach($data as $key){
+            $this->data[] = ['name' => $key[1],
+                ['data' => $this->GetAnswers($key[0])]
+            ];
+        }
+
+        if($this->data){
+           return $this->data;
+        }else{
+            return false;
+        }
+    }
+
+    public function GetAnswers($q_id){
+        $sql = "SELECT * FROM answers WHERE q_id ='$q_id'";
+
+        $result = mysqli_query(Db::connect(),$sql) or die('mysql error');
+
+        if($result = mysqli_fetch_all($result)){
+            return $result;
+        } else{
+            return null;
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 require_once ('class/questions.php');
-
-$q = new Questions();
-$q->GetQuestions();
-
 require_once ('class/session.php');
 require_once ('class/user.php');
 session_start();
@@ -14,6 +10,22 @@ if(is_null($_SESSION['id'])){
     header("location: user_form.php");
 }
 echo "WELLCOME TO THE VICTORINA ".$_SESSION['name'];
+echo "<hr>";
+
+$q = new Questions();
+$data = $q->GetQuestions();
+echo "<pre>";
+for($i=0;$i<count($data);$i++){
+    echo($data[$i]['name'])."<br>";
+    echo "<hr>";
+    echo "<hr>";
+    for($j=0;$j<count($data[$i][0]['data']);$j++){
+        echo "<hr>";
+        print_r($data[$i][0]['data'][$j][2]);
+        echo "<br>";
+    }
+    echo "<hr>";
+}
 
 
 ?>
