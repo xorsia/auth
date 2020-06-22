021455f0f5c51b44f6cd5c64edaa843fd8b0173b
Xorsia <thismailforhacker@gmail.com>
2020-06-22 21:12:32 +0300

    реализовую подсчет результата после теста

--- index.php
+++ index.php
@@ -9,26 +9,25 @@ if(is_null($_SESSION['id'])){
 } elseif ($user->GetSessionId($_SESSION['name']) != $_SESSION['id']){
     header("location: user_form.php");
 }
-echo "WELLCOME TO THE VICTORINA ".$_SESSION['name'];
-echo "<br>";
-
 $q = new Questions();
 $data = $q->GetQuestions();
 
+echo "WELLCOME TO THE VICTORINA ".$_SESSION['name'];
+echo "<br>";
+
+echo "<form method='Post' action='result.php'>";
 for($i=0;$i<count($data);$i++){
     echo "<p>".($data[$i]['name'])."</p><br>";
-
     for($j=0;$j<count($data[$i][0]['data']);$j++){
-        echo "<form method='post'>";
-        echo "<input type='checkbox' name='answer' value='0 .'>".$data[$i][0]['data'][$j][2];
+        $value = $data[$i][0]['data'][$j][3];
+        $input_name  = "answer_".$i;
+        echo "<input type='radio' name=$input_name value=$value />".$data[$i][0]['data'][$j][2];
         echo "<br>";
-        echo "</form>";
-
-//        print_r($data[$i][0]['data'][$j][2]);
-
         echo "<br>";
     }
     echo "<hr>";
 }
+echo "<input type='submit' name='send_test' value='закончить тест'>";
+echo "</form>";
 ?>
 
